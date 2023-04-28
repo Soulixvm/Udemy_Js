c47acb0df8daa5b948f1af0796a80b1378fb2010
Soulaiman <soulaiman.bouazaiz@hotmail.fr>
2023-04-28 16:07:53 +0200

    practice assignement

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -17,3 +17,16 @@
 // console.log(firstName);
 
 
+
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country(population in millions)
+// 2. Log their values to the console
+
+let country = "Belgium"
+let continent = "Europe"
+let population = "10"
+
+console.log("country");
+console.log("continent");
+console.log("population");
