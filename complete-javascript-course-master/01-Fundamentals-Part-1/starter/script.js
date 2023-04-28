0abeeb78a70f7aeb4938804b61c5a035e618cf52
Soulaiman <soulaiman.bouazaiz@hotmail.fr>
2023-04-28 15:56:55 +0200

    values and variables

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,19 @@
-// linking js file
+// !!!linking js file!!!
+
 // let js = 'amazing';
 // if (js === 'amazing') alert('hello')
-
 // console.log(40 + 8 + 23 + 10)
+
+
+
+// !!!values & variables!!!
+
+// console.log('jonas');
+// console.log(23);
+
+// let firstName = 'Matilda';
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
 
