f4a7d23e1cb774b5edbfaa5482b19f61bf2b80a2
Soulaiman <soulaiman.bouazaiz@hotmail.fr>
2023-05-02 14:29:28 +0200

    Coding challenge #3

--- complete-javascript-course-master/01-Fundamentals-Part-1/Coding-Challenge-1/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/Coding-Challenge-1/script.js
@@ -27,4 +27,48 @@
 //     console.log(`Jhons BMI ${JhonBmi} is greater than Marks one ${MarkBmi}`);
 // }
 
-/* Coding Challenge #2 */
+/* Coding Challenge #3 */
+
+//data 1
+// let dolphinsFirstScore = 96;
+// let dolphinsSecondScore = 108;
+// let dolphinsThirdScore = 89;
+// let koalasFirstScore = 88;
+// let koalasSecondScore = 91;
+// let koalasThirdScore = 110;
+let dolphinsFirstScore = 400;
+let dolphinsSecondScore = 400;
+let dolphinsThirdScore = 400;
+let koalasFirstScore = 400;
+let koalasSecondScore = 400;
+let koalasThirdScore = 400;
+
+let dolphinsAvg = AvgScore(dolphinsFirstScore, dolphinsSecondScore, dolphinsThirdScore);
+let koalasAvg = AvgScore(koalasFirstScore, koalasSecondScore, koalasThirdScore);
+
+function AvgScore(n1, n2, n3) {
+    return (Number(n1) + Number(n2) + Number(n3)) / 3
+}
+
+//Bonus 1
+
+if (dolphinsAvg > koalasAvg && dolphinsAvg >= 100) {
+    console.log(`Dolphins wins!`);
+} else if (dolphinsAvg < koalasAvg && koalasAvg >= 100) {
+    console.log(`Koalas Wins!`);
+} else {
+    console.log(`Both Wins!`);
+}
+
+//Bonus 2
+
+if (dolphinsAvg > koalasAvg && dolphinsAvg >= 100) {
+    console.log(`Dolphins wins!`);
+} else if (dolphinsAvg < koalasAvg && koalasAvg >= 100) {
+    console.log(`Koalas Wins!`);
+} else if (dolphinsAvg === koalasAvg && dolphinsAvg >= 100 && koalasAvg >= 100) {
+    console.log(`Both Wins!`);
+} else {
+    console.log(`No Team wins de trophy!`);
+}
+
