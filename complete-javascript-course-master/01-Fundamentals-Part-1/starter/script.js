8d2753b301c07d1e9c45b64f42de83c5ad1c5eb7
Soulaiman <soulaiman.bouazaiz@hotmail.fr>
2023-05-02 11:46:33 +0200

    faly values

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -100,3 +100,24 @@ console.log(century);
 // console.log('23' * '2');
 // console.log('23' / '2');
 
+//!!!falsy values undefined, null, NaN!!!
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+let money = 0;
+if (money) {
+    console.log(`Don't spend it all`);
+} else {
+    console.log(`You should get a job!`);
+}
+
+let height;
+
+if (height) {
+    console.log("Yay! height is defined");
+} else {
+    console.log(`Height is UNDEFINED`);
+}
