d8078e6f1885d6f9ed4b8d5f514ff6c5090ffdf8
SoulaimanBouazaiz <soulaiman.bouazaiz@hotmail.fr>
2023-05-12 09:13:28 +0200

    conding challenge #4

--- complete-javascript-course-master/01-Fundamentals-Part-1/Coding-Challenge-1/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/Coding-Challenge-1/script.js
@@ -72,3 +72,13 @@
 //     console.log(`No Team wins de trophy!`);
 // }
 
+/* Coding challenge #4*/
+// let bill = 275;
+// let bill = 40;
+// let bill = 430;
+
+// let tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+
+// let totalValue = bill + tip
+
+// console.log(`The bill was ${bill}€, the tip was ${tip}€ and the total cost was ${totalValue}€.`);
